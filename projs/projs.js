7679760184d5b5c2a03a6939e24a1d389b077b43
DannyGrimmig <dannygrimmig17@gmail.com>
2023-02-15 12:04:09 -0500

    projects: add tools used for projects

--- projs/projs.js
+++ projs/projs.js
@@ -6,6 +6,11 @@ let fsBio = document.getElementById("fsBio");
 let feBio = document.getElementById("feBio");
 let beBio = document.getElementById("beBio");
 
+let toolsContainer = document.getElementsByClassName("tools");
+let html = document.createElement("span"); html.innerHTML = "HTML";
+let css = document.createElement("span"); css.innerHTML = "CSS";
+let js = document.createElement("span"); js.innerHTML = "JS";
+
 images.forEach(function(image, index){
     image.addEventListener("mouseover", function(){
         console.log(index);
@@ -16,6 +21,18 @@ images.forEach(function(image, index){
         else if(index>=2 && index<=5){
             feTitle.innerHTML = image.id;
             feBio.innerHTML = image.name;
+
+            if(index < 4){
+                toolsContainer[1].appendChild(html);
+                toolsContainer[1].appendChild(css);
+                toolsContainer[1].appendChild(js);
+            }
+            else{
+                toolsContainer[1].innerHTML = "";
+                toolsContainer[1].appendChild(html);
+                toolsContainer[1].appendChild(css);
+            }
+           
         }
         else{
             beTitle.innerHTML = image.id;
